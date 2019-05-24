92c5a0dbc1b11368e12aace4b4f1549d8f42b29c
Slate <1815846446@qq.com>
2019-05-24 17:35:33 +0800

    v1.2改进界面，使用cardview美化布局，美化点击响应样式

--- app/src/main/java/com/example/slatejack/diandianapp/MainActivity.java
+++ app/src/main/java/com/example/slatejack/diandianapp/MainActivity.java
@@ -11,97 +11,107 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    public static  final String TAG=MainActivity.class.getCanonicalName();
-    private List<String> mList=new ArrayList<>();
-
-
+    public static final String TAG = MainActivity.class.getCanonicalName();
+    private List<String> mList = new ArrayList<>();
+    private List<String> mList1 = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
         initData();
+        initData1();
         initView();
-
-
     }
 
     private void initData() {
-        mList.add("cctv1");
-        mList.add("cctv1 高清");
-        mList.add("cctv2 高清");
-        mList.add("cctv3");
-        mList.add("cctv3 高清");
-        mList.add("cctv4");
-        mList.add("cctv4 高清");
-        mList.add("cctv5");
-        mList.add("cctv5 高清");
+        mList.add( "测试平台" );
+        mList.add( "cctv1 高清" );
+        mList.add( "cctv2 高清" );
+        mList.add( "cctv3" );
+        mList.add( "cctv3 高清" );
+        mList.add( "cctv4" );
+        mList.add( "cctv4 高清" );
+        mList.add( "cctv5" );
+        mList.add( "cctv5 高清" );
     }
+
+    private void initData1() {
+        mList1.add( "测试平台仅供参考测试使用" );
+        mList1.add( "欢迎收看cctv1高清(1080p)" );
+        mList1.add( "欢迎收看cctv2高清(1080p)" );
+        mList1.add( "欢迎收看cctv3" );
+        mList1.add( "欢迎收看cctv3高清(1080p)" );
+        mList1.add( "欢迎收看cctv4" );
+        mList1.add( "欢迎收看cctv4高清(1080p)" );
+        mList1.add( "欢迎收看cctv5" );
+        mList1.add( "欢迎收看cctv5高清(1080p)" );
+    }
+
     private void initView() {
-        VerticalAdapter adapter = new VerticalAdapter(this);
-        RecyclerView rcvVertical = findViewById(R.id.rcv_vertical);
+        VerticalAdapter adapter = new VerticalAdapter( this );
+        RecyclerView rcvVertical = findViewById( R.id.rcv_vertical );
 
-        LinearLayoutManager managerVertical = new LinearLayoutManager(this);
-        managerVertical.setOrientation(LinearLayoutManager.VERTICAL);
+        LinearLayoutManager managerVertical = new LinearLayoutManager( this );
+        managerVertical.setOrientation( LinearLayoutManager.VERTICAL );
 
-        rcvVertical.setLayoutManager(managerVertical);
-        rcvVertical.setHasFixedSize(true);
-        rcvVertical.setAdapter(adapter);
-
-        adapter.setVerticalDataList(mList);
-
-        adapter.setOnItemClickListener(new VerticalAdapter.OnItemClickListener() {
+        rcvVertical.setLayoutManager( managerVertical );
+        rcvVertical.setHasFixedSize( true );
+        rcvVertical.setAdapter( adapter );
+        adapter.setVerticalDataList( mList,mList1);
+        adapter.setOnItemClickListener( new VerticalAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
                 //定义相应的事件逻辑
-                switch (position){
+                switch (position) {
                     case 0: {
                         Intent intent1 = new Intent( MainActivity.this, cctv1.class );
                         MainActivity.this.startActivity( intent1 );
                         break;
                     }
-                    case 1:{
+                    case 1: {
                         Intent intent2 = new Intent( MainActivity.this, cctv1HD.class );
                         MainActivity.this.startActivity( intent2 );
                         break;
                     }
-                    case 2:{
+                    case 2: {
                         Intent intent3 = new Intent( MainActivity.this, cctv2HD.class );
                         MainActivity.this.startActivity( intent3 );
                         break;
                     }
-                    case 3:{
+                    case 3: {
                         Intent intent4 = new Intent( MainActivity.this, cctv3.class );
                         MainActivity.this.startActivity( intent4 );
                         break;
                     }
-                    case 4:{
+                    case 4: {
                         Intent intent5 = new Intent( MainActivity.this, cctv3HD.class );
                         MainActivity.this.startActivity( intent5 );
                         break;
                     }
-                    case 5:{
+                    case 5: {
                         Intent intent6 = new Intent( MainActivity.this, cctv4.class );
                         MainActivity.this.startActivity( intent6 );
                         break;
                     }
-                    case 6:{
+                    case 6: {
                         Intent intent7 = new Intent( MainActivity.this, cctv4HD.class );
                         MainActivity.this.startActivity( intent7 );
                         break;
                     }
-                    case 7:{
+                    case 7: {
                         Intent intent8 = new Intent( MainActivity.this, cctv5.class );
                         MainActivity.this.startActivity( intent8 );
                         break;
                     }
-                    case 8:{
+                    case 8: {
                         Intent intent9 = new Intent( MainActivity.this, cctv5HD.class );
                         MainActivity.this.startActivity( intent9 );
                         break;
                     }
                 }
             }
-        });
+        } );
 
     }
+
 }
--- app/src/main/java/com/example/slatejack/diandianapp/VerticalAdapter.java
+++ app/src/main/java/com/example/slatejack/diandianapp/VerticalAdapter.java
@@ -1,11 +1,13 @@
 package com.example.slatejack.diandianapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,25 +24,25 @@ public class VerticalAdapter extends RecyclerView.Adapter<VerticalAdapter.Vertic
     private static final String TAG = VerticalAdapter.class.getSimpleName();//得到类的简写名称，即VerticalAdapter
     private Context mContext;//实例化抽象类（包含activity等抽象对象）
     private List<String> mList = new ArrayList<>();//实例化对象
+    private List<String> mList1 = new ArrayList<>();//实例化对象
 
+    private OnItemClickListener itemClickListener;
     public interface OnItemClickListener{
         void onItemClick(View view, int position);//该方法回调到activity或fragment中做相应操作
     }
-
-    private OnItemClickListener itemClickListener;
-
     public void setOnItemClickListener(OnItemClickListener onItemClickListener ){
         this.itemClickListener=onItemClickListener;
     }
-
     public VerticalAdapter(Context context) {//传入参数，形参等于实参
-        mContext = context;
-    }
-    
 
-    public void setVerticalDataList(List<String> list) {
-        Log.d(TAG, "setVerticalDataList: " + list.size());//创建日志
+        mContext = context;
+
+    }
+
+    public void setVerticalDataList(List<String> list,List<String> list1) {
+        Log.d(TAG, "setVerticalDataList: " + list.size());//创建日志,方便报错查看
         mList = list;
+        mList1 = list1;
         notifyDataSetChanged();//动态更新方法调用
     }
 
@@ -55,33 +57,36 @@ public class VerticalAdapter extends RecyclerView.Adapter<VerticalAdapter.Vertic
     public void onBindViewHolder(@NonNull VerticalViewHolder holder, int position) {
         holder.tvNum.setText(position + 1 + "");//设置数字每一次加入一个新的list就加1
         holder.tvContent.setText(mList.get(position));//设置显示内容为list内的文字
-
+        holder.tvIn.setText( mList1.get( position ) );
 
     }
 
     @Override
     public int getItemCount() {
         return mList == null ? 0 : mList.size();//返回list长度
+
     }
 
     public class VerticalViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView tvNum, tvContent;//实例化xml文件中的两个文本控件
-        View tvview;
-        OnItemClickListener mitemClickListener;
+        TextView tvNum, tvContent,tvIn;//实例化xml文件中的两个文本控件
+        CardView cardView;
+        ImageView imageView;
+        OnItemClickListener mitemClickListener;//实例化监听器
         public VerticalViewHolder(View itemView) {
-
             super(itemView);
             tvNum = itemView.findViewById(R.id.tv_num);
             tvContent = itemView.findViewById(R.id.tv_content);
+            tvIn=itemView.findViewById( R.id.tv_in );
+            cardView=itemView.findViewById( R.id.card );
+            imageView=itemView.findViewById( R.id.imageView );
             this.mitemClickListener=itemClickListener;
-            itemView.setOnClickListener(this);
+            itemView.setOnClickListener(this);//设置监听器
 
         }
-
         @Override
         public void onClick(View v) {
             if (mitemClickListener != null) {
-                mitemClickListener.onItemClick(itemView,getPosition());
+                mitemClickListener.onItemClick(itemView,getPosition());//获取到栏目条的地址，从0开始
             }
 
         }
